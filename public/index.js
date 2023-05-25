e171568674b93aedee5534f8e76c88f3e0546008
dev-talib <mohdtalib.dev@gmail.com>
2023-05-25 18:02:10 +0530

    Update view and Add functionality of stop pause and mute video

--- public/index.js
+++ public/index.js
@@ -2,7 +2,7 @@ const socket = io('/');
 const peer = new Peer();
 let myVideoStream;
 let myId;
-var videoGrid = document.getElementById('videoDiv')
+var videoGrid = document.getElementById('video-grid')
 var myvideo = document.createElement('video');
 myvideo.muted = true;
 const peerConnections = {}
@@ -64,4 +64,64 @@ function addVideo(video , stream){
     video.play()
   })
   videoGrid.append(video);
+}
+
+
+
+
+// extra functionality
+
+const muteUnmute = () => {
+  const enabled = myVideoStream.getAudioTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getAudioTracks()[0].enabled = false;
+    setUnmuteButton();
+  } else {
+    setMuteButton();
+    myVideoStream.getAudioTracks()[0].enabled = true;
+  }
+}
+
+const playStop = () => {
+  console.log('object')
+  let enabled = myVideoStream.getVideoTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getVideoTracks()[0].enabled = false;
+    setPlayVideo()
+  } else {
+    setStopVideo()
+    myVideoStream.getVideoTracks()[0].enabled = true;
+  }
+}
+
+const setMuteButton = () => {
+  const html = `
+    <i class="fas fa-microphone"></i>
+    <span>Mute</span>
+  `
+  document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const setUnmuteButton = () => {
+  const html = `
+    <i class="unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>
+  `
+  document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const setStopVideo = () => {
+  const html = `
+    <i class="fas fa-video"></i>
+    <span>Stop Video</span>
+  `
+  document.querySelector('.main__video_button').innerHTML = html;
+}
+
+const setPlayVideo = () => {
+  const html = `
+  <i class="stop fas fa-video-slash"></i>
+    <span>Play Video</span>
+  `
+  document.querySelector('.main__video_button').innerHTML = html;
 }
