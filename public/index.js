3f9730f5ddd5a39276b018162d824628dcce73cc
dev-talib <mohdtalib.dev@gmail.com>
2023-05-26 14:34:34 +0530

    Add comments

--- public/index.js
+++ public/index.js
@@ -1,127 +1,93 @@
-const socket = io('/');
-const peer = new Peer();
-let myVideoStream;
-let myId;
-var videoGrid = document.getElementById('video-grid')
-var myvideo = document.createElement('video');
-myvideo.muted = true;
-const peerConnections = {}
+const socket = io('/'); // Establish a socket connection to the server
+
+const peer = new Peer(); // Create a new Peer instance
+
+let myVideoStream; // Variable to store the user's video stream
+let myId; // Variable to store the user's ID
+
+var videoGrid = document.getElementById('video-grid'); // Get the video grid element
+var myvideo = document.createElement('video'); // Create a video element for the user's video
+myvideo.muted = true; // Mute the user's own video
+
+const peerConnections = {}; // Object to store the peer connections
+
+// Request user's media (video and audio) stream
 navigator.mediaDevices.getUserMedia({
-  video:true,
-  audio:true
-}).then((stream)=>{
-  myVideoStream = stream;
-  addVideo(myvideo , stream);
-  peer.on('call' , call=>{
-    call.answer(stream);
-      const vid = document.createElement('video');
-    call.on('stream' , userStream=>{
-      addVideo(vid , userStream);
-    })
-    call.on('error' , (err)=>{
-      alert(err)
-    })
+  video: true,
+  audio: true
+}).then((stream) => {
+  myVideoStream = stream; // Store the user's media stream
+  addVideo(myvideo, stream); // Add user's video to the video grid
+
+  peer.on('call', (call) => {
+    call.answer(stream); // Answer incoming call with the user's stream
+
+    const vid = document.createElement('video'); // Create a video element for the remote user
+    call.on('stream', (userStream) => {
+      addVideo(vid, userStream); // Add remote user's video to the video grid
+    });
+
+    call.on('error', (err) => {
+      alert(err); // Display error if call encounters an error
+    });
+
     call.on("close", () => {
-        console.log(vid);
-        vid.remove();
-    })
-    peerConnections[call.peer] = call;
-  })
-}).catch(err=>{
-    alert(err.message)
-})
-peer.on('open' , (id)=>{
-  myId = id;
-  socket.emit("newUser" , id , roomID);
-})
-peer.on('error' , (err)=>{
-  alert(err.type);
+      console.log(vid);
+      vid.remove(); // Remove the video element when call is closed
+    });
+
+    peerConnections[call.peer] = call; // Store the call object in the peer connections object
+  });
+}).catch((err) => {
+  alert(err.message); // Display error if accessing media devices fails
 });
-socket.on('userJoined' , id=>{
-  console.log("new user joined")
-  const call  = peer.call(id , myVideoStream);
-  const vid = document.createElement('video');
-  call.on('error' , (err)=>{
-    alert(err);
-  })
-  call.on('stream' , userStream=>{
-    addVideo(vid , userStream);
-  })
-  call.on('close' , ()=>{
-    vid.remove();
-    console.log("user disconect")
-  })
-  peerConnections[id] = call;
-})
-socket.on('userDisconnect' , id=>{
-  if(peerConnections[id]){
-    peerConnections[id].close();
+
+peer.on('open', (id) => {
+  myId = id; // Store the user's peer ID
+  socket.emit("newUser", id, roomID); // Notify the server that a new user has joined with their ID and room ID
+});
+
+peer.on('error', (err) => {
+  alert(err.type); // Display error if there is a Peer error
+});
+
+socket.on('userJoined', (id) => {
+  console.log("new user joined");
+  const call = peer.call(id, myVideoStream); // Initiate a call to the newly joined user
+
+  const vid = document.createElement('video'); // Create a video element for the remote user
+  call.on('error', (err) => {
+    alert(err); // Display error if call encounters an error
+  });
+
+  call.on('stream', (userStream) => {
+    addVideo(vid, userStream); // Add remote user's video to the video grid
+  });
+
+  call.on('close', () => {
+    vid.remove(); // Remove the video element when call is closed
+    console.log("user disconnected");
+  });
+
+  peerConnections[id] = call; // Store the call object in the peer connections object
+});
+
+socket.on('userDisconnect', (id) => {
+  if (peerConnections[id]) {
+    peerConnections[id].close(); // Close the peer connection when a user disconnects
   }
-})
-function addVideo(video , stream){
+});
+
+// Function to add video to the video grid
+function addVideo(video, stream) {
   video.srcObject = stream;
   video.addEventListener('loadedmetadata', () => {
-    video.play()
-  })
-  videoGrid.append(video);
+    video.play();
+  });
+  videoGrid.append(video); // Add the video element to the video grid
 }
 
+// Extra functionality for mute/unmute and play/stop video buttons
 
-
-
-// extra functionality
-
-const muteUnmute = () => {
-  const enabled = myVideoStream.getAudioTracks()[0].enabled;
-  if (enabled) {
-    myVideoStream.getAudioTracks()[0].enabled = false;
-    setUnmuteButton();
-  } else {
-    setMuteButton();
-    myVideoStream.getAudioTracks()[0].enabled = true;
-  }
-}
-
-const playStop = () => {
-  console.log('object')
-  let enabled = myVideoStream.getVideoTracks()[0].enabled;
-  if (enabled) {
-    myVideoStream.getVideoTracks()[0].enabled = false;
-    setPlayVideo()
-  } else {
-    setStopVideo()
-    myVideoStream.getVideoTracks()[0].enabled = true;
-  }
-}
-
-const setMuteButton = () => {
-  const html = `
-    <i class="fas fa-microphone"></i>
-    <span>Mute</span>
-  `
-  document.querySelector('.main__mute_button').innerHTML = html;
-}
-
-const setUnmuteButton = () => {
-  const html = `
-    <i class="unmute fas fa-microphone-slash"></i>
-    <span>Unmute</span>
-  `
-  document.querySelector('.main__mute_button').innerHTML = html;
-}
-
-const setStopVideo = () => {
-  const html = `
-    <i class="fas fa-video"></i>
-    <span>Stop Video</span>
-  `
-  document.querySelector('.main__video_button').innerHTML = html;
-}
-
-const setPlayVideo = () => {
-  const html = `
-  <i class="stop fas fa-video-slash"></i>
-    <span>Play Video</span>
-  `
-  document.querySelector('.main__video_button').innerHTML = html;
-}
+// Function to mute/unmute the user's audio
+const muteUnmute = ()
--- server.js
+++ server.js
@@ -3,32 +3,42 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const port = process.env.PORT || 4000;
-const {v4:uuidv4} = require('uuid');
-const {ExpressPeerServer} = require('peer')
-const peer = ExpressPeerServer(server , {
-  debug:true
+const { v4: uuidv4 } = require('uuid');
+const { ExpressPeerServer } = require('peer');
+const peer = ExpressPeerServer(server, {
+  debug: true
 });
 app.use('/peerjs', peer);
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
-
-app.get('/' , (req,res)=>{
+// Redirect to a unique room when the root URL is accessed
+app.get('/', (req, res) => {
   res.redirect(`/${uuidv4()}`);
 });
 
-app.get('/:room' , (req,res)=>{
-    res.render('index' , {RoomId:req.params.room});
+// Render the index.ejs file and pass the RoomId parameter
+app.get('/:room', (req, res) => {
+  res.render('index', { RoomId: req.params.room });
 });
-io.on("connection" , (socket)=>{
-  socket.on('newUser' , (id , room)=>{
+
+// Handle socket.io connections
+io.on("connection", (socket) => {
+  // When a new user joins a room
+  socket.on('newUser', (id, room) => {
     socket.join(room);
-    socket.to(room).broadcast.emit('userJoined' , id);
-    socket.on('disconnect' , ()=>{
-        socket.to(room).broadcast.emit('userDisconnect' , id);
-    })
-  })
-})
-server.listen(port , ()=>{
-  console.log("Server running on port : " + port);
-})
+    // Notify other users in the room that a new user has joined
+    socket.to(room).broadcast.emit('userJoined', id);
+    
+    // Handle disconnection of a user
+    socket.on('disconnect', () => {
+      // Notify other users in the room that a user has disconnected
+      socket.to(room).broadcast.emit('userDisconnect', id);
+    });
+  });
+});
+
+// Start the server and listen on the specified port
+server.listen(port, () => {
+  console.log("Server running on port: " + port);
+});
